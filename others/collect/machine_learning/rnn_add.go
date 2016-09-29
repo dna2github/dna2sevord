a705ce5866177c220f90d3a99daa65ab2ef904a3
Seven Lju <dna2gle@gmail.com>
2016-09-29 16:35:19 +0800

    [#others.collect] basic rnn add
    - refactory for more readable

--- others/collect/machine_learning/rnn_add.go
+++ others/collect/machine_learning/rnn_add.go
@@ -88,6 +88,41 @@ func (X *SimpleMatrix) Col (x int) *SimpleMatrix {
    return R
 }
 
+func (X *SimpleMatrix) MirrorM () *SimpleMatrix {
+   n := X.N - 1
+   R := NewSimpleMatrix(X.M, X.N)
+   for i := X.M - 1; i >= 0; i-- {
+      for j := X.N - 1; j >= 0; j-- {
+         R.Data[i][j] = X.Data[i][n - j]
+      }
+   }
+   return R
+}
+
+func (X *SimpleMatrix) MirrorN () *SimpleMatrix {
+   m := X.M - 1
+   R := NewSimpleMatrix(X.M, X.N)
+   for i := X.M - 1; i >= 0; i-- {
+      for j := X.N - 1; j >= 0; j-- {
+         R.Data[i][j] = X.Data[m - i][j]
+      }
+   }
+   return R
+}
+
+func (X *SimpleMatrix) EltMul(Y *SimpleMatrix) *SimpleMatrix {
+   if X.M != Y.M && X.N != Y.N {
+      return nil
+   }
+   R := NewSimpleMatrix(X.M, X.N)
+   for i := X.M - 1; i >= 0; i-- {
+      for j := X.N - 1; j >= 0; j-- {
+         R.Data[i][j] = X.Data[i][j] * Y.Data[i][j]
+      }
+   }
+   return R
+}
+
 func (X *SimpleMatrix) Dot(Y *SimpleMatrix) *SimpleMatrix {
    if X.N != Y.M {
       return nil
@@ -128,43 +163,38 @@ func (X *SimpleMatrix) Scale(a float64) *SimpleMatrix {
    return R
 }
 
-
 type RecurrentNerualNetwork struct {
    Alpha float64
    InputDim, HiddenDim, OutputDim int
    Synapse0, Synapse1, SynapseH, Tmp *SimpleMatrix
-   Sigmoid func (x float64) float64
 }
 
 func sigmoid(x float64) float64 {
    return 1.0/(1.0 + math.Exp(x))
 }
 
+func sigmoid_backward(x float64) float64 {
+   return x*(1 - x)
+}
+
 func add(r float64, x float64) float64 {
    return r + x
 }
 
-func sigmoid_output_to_derivative(x float64) float64 {
-   return x*(1 - x)
-}
-
-func NewRecurrentNerualNetwork (
-      f func (float64) float64, alpha float64, indim, hiddendim, outdim int) *RecurrentNerualNetwork {
+func NewRecurrentNerualNetwork (alpha float64, indim, hiddendim, outdim int) *RecurrentNerualNetwork {
    rnn := new(RecurrentNerualNetwork)
    rnn.Alpha = alpha
    rnn.InputDim = indim
    rnn.HiddenDim = hiddendim
    rnn.OutputDim = outdim
-   rnn.Sigmoid = f
    rnn.Synapse0 = NewSimpleMatrix(rnn.InputDim, rnn.HiddenDim)
    rnn.Synapse1 = NewSimpleMatrix(rnn.HiddenDim, rnn.OutputDim)
    rnn.SynapseH = NewSimpleMatrix(rnn.HiddenDim, rnn.HiddenDim)
    return rnn
 }
 
-func (rnn *RecurrentNerualNetwork) Train(in *SimpleMatrix, out *SimpleMatrix) {
+func (rnn *RecurrentNerualNetwork) Train(in *SimpleMatrix, out *SimpleMatrix) float64 {
    // in = M*N, out = 1*N
-   m   := in.M
    n   := out.N
    err := 0.0
    layer_2_deltas := make([]*SimpleMatrix, 0)
@@ -172,30 +202,19 @@ func (rnn *RecurrentNerualNetwork) Train(in *SimpleMatrix, out *SimpleMatrix) {
    layer_1_values = append(layer_1_values, NewSimpleMatrix(1, rnn.HiddenDim))
    current := NewSimpleMatrix(out.M, out.N)
    for i := 0; i < n; i++ {
-      X := NewSimpleMatrix(1, m)
-      for j := 0; j < m; j++ {
-         X.Data[0][j] = in.Data[j][in.N - i - 1]
-      }
-      y := NewSimpleMatrix(1, out.M)
-      for j := 0; j < out.M; j++ {
-         y.Data[0][j] = out.Data[j][out.N - i - 1]
-      }
-      layer_1 := X.Dot(rnn.Synapse0).Add(layer_1_values[len(layer_1_values) - 1].Dot(rnn.SynapseH), 1, 1)
-      layer_1 = layer_1.Map(sigmoid)
+      X := in.Col(in.N - i - 1).T()
+      y := out.Col(out.N - i - 1).T()
+      layer_1 := X.Dot(rnn.Synapse0).Add(layer_1_values[len(layer_1_values) - 1].Dot(rnn.SynapseH), 1, 1).Map(sigmoid)
       layer_2 := layer_1.Dot(rnn.Synapse1).Map(sigmoid)
       layer_2_error := y.Add(layer_2, 1, -1)
-      layer_2_delta := NewSimpleMatrix(layer_2_error.M, layer_2_error.N)
-      for j := layer_2_error.M - 1; j >= 0; j-- {
-         for k := layer_2_error.N - 1; k >= 0; k-- {
-            layer_2_delta.Data[j][k] = layer_2_error.Data[j][k] * sigmoid_output_to_derivative(layer_2.Data[j][k])
-         }
-      }
+      layer_2_delta := layer_2_error.EltMul(layer_2.Map(sigmoid_backward))
       layer_2_deltas = append(layer_2_deltas, layer_2_delta)
+      layer_1_values = append(layer_1_values, layer_1)
+
       err += layer_2_error.Map(math.Abs).Reduce(add, 0)
       for j := out.M - 1; j >= 0; j-- {
          current.Data[j][i] = layer_2.Data[0][j]
       }
-      layer_1_values = append(layer_1_values, layer_1.Clone())
    }
    rnn.Tmp = current
 
@@ -204,21 +223,13 @@ func (rnn *RecurrentNerualNetwork) Train(in *SimpleMatrix, out *SimpleMatrix) {
    s1update := NewSimpleMatrix(rnn.Synapse1.M, rnn.Synapse1.N)
    shupdate := NewSimpleMatrix(rnn.SynapseH.M, rnn.SynapseH.N)
    for i := 0; i < n; i++ {
-      X := NewSimpleMatrix(1, m)
-      for j := 0; j < m; j++ {
-         X.Data[0][j] = in.Data[j][i]
-      }
+      X := in.Col(i).T()
       layer_1 := layer_1_values[len(layer_1_values) - i - 1]
       prev_layer_1 := layer_1_values[len(layer_1_values) - i - 2]
       layer_2_delta := layer_2_deltas[len(layer_2_deltas) - i - 1]
       future_layer_1_delta = future_layer_1_delta.Dot(rnn.SynapseH.T())
       layer_2_delta = layer_2_delta.Dot(rnn.Synapse1.T())
-      layer_1_delta := future_layer_1_delta.Add(layer_2_delta, 1, 1)
-      for j := layer_1_delta.M - 1; j >= 0; j-- {
-         for k := layer_1_delta.N - 1; k >= 0; k-- {
-            layer_1_delta.Data[j][k] *= sigmoid_output_to_derivative(layer_1.Data[j][k])
-         }
-      }
+      layer_1_delta := future_layer_1_delta.Add(layer_2_delta, 1, 1).EltMul(layer_1.Map(sigmoid_backward))
       s0update = s0update.Add(X.T().Dot(layer_1_delta), 1, 1)
       s1update = s1update.Add(layer_1.T().Dot(layer_2_delta), 1, 1)
       shupdate = shupdate.Add(prev_layer_1.T().Dot(layer_1_delta), 1, 1)
@@ -226,6 +237,7 @@ func (rnn *RecurrentNerualNetwork) Train(in *SimpleMatrix, out *SimpleMatrix) {
    rnn.Synapse0 = rnn.Synapse0.Add(s0update, 1, rnn.Alpha)
    rnn.Synapse1 = rnn.Synapse1.Add(s1update, 1, rnn.Alpha)
    rnn.SynapseH = rnn.SynapseH.Add(shupdate, 1, rnn.Alpha)
+   return err
 }
 
 func (rnn *RecurrentNerualNetwork) Dump () {
@@ -253,13 +265,21 @@ func prepareBinaryMap() map[int][]int {
 func convertBits (bits *SimpleMatrix) []int {
    r := make([]int, 8)
    for i := 0; i < 8; i++ {
-      r[7 - i] = int(bits.Data[0][i] + 0.5)
+      r[i] = int(bits.Data[0][i] + 0.5)
    }
    return r
 }
 
+func decodeNum (bits []int) int {
+   r := 0.0
+   for i := 0; i < 8; i++ {
+      r += math.Pow(2.0, float64(i)) * float64(bits[7 - i])
+   }
+   return int(r)
+}
+
 func main () {
-   rnn := NewRecurrentNerualNetwork(sigmoid, 0.1, 2, 16, 1)
+   rnn := NewRecurrentNerualNetwork(0.1, 2, 16, 1)
    rnn.Synapse0.FillRandom(-1, 1)
    rnn.Synapse1.FillRandom(-1, 1)
    rnn.SynapseH.FillRandom(-1, 1)
@@ -278,9 +298,13 @@ func main () {
          in.Data[1][j] = float64(b[j])
          out.Data[0][j] = float64(c[j])
       }
-      rnn.Train(in, out)
+      err := rnn.Train(in, out)
       if (i % 1000 == 0) {
-         fmt.Println(a, " + ", b, " = ", c, " A: ", convertBits(rnn.Tmp))
+         fmt.Println("Error: [", err, "]")
+         fmt.Println("Pred:", convertBits(rnn.Tmp.MirrorM()))
+         fmt.Println("True:", c)
+         fmt.Println(decodeNum(a), "+", decodeNum(b), "=", decodeNum(convertBits(rnn.Tmp.MirrorM())) )
+         fmt.Println("------------")
       }
    }
 }
